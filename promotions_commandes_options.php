61f14cccd53ea23cb331a1c8df3d3bb07f14c2c5
Rainer Müller <rainer@websimple.be>
2018-03-25 13:40:18 +0200

    ajoutem les commandes aux promotions

--- promotions_commandes_options.php
+++ promotions_commandes_options.php
@@ -14,11 +14,5 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 }
 
 
-/*
- * Un fichier d'options permet de définir des éléments
- * systématiquement chargés à chaque hit sur SPIP.
- *
- * Il vaut donc mieux limiter au maximum son usage
- * tout comme son volume !
- * 
- */
+// Ajoute le plugin commandes aux promotions.
+$GLOBALS['promotion_plugin']['commandes'] = _T('commandes:commandes_titre');
