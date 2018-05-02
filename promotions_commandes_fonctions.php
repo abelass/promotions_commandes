099d6c85c4aaf3d2643dc26871168297c244b160
abelass <rainer@websimple.be>
2018-05-02 09:24:48 +0200

    format

--- promotions_commandes_fonctions.php
+++ promotions_commandes_fonctions.php
@@ -19,5 +19,5 @@ if (!defined('_ECRIRE_INC_VERSION')) {
  * systématiquement chargés lors du calcul des squelettes.
  *
  * Il peut par exemple définir des filtres, critères, balises, …
- * 
+ *
  */
